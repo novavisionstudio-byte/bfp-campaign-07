089149ec3ccf46cacac870a70cc9942e4c999db9
campaign <campaign@thesis.local>
2026-07-10 17:03:00 +0100

    [011/risky] feat: large module gen_11 (light tests)

---
+++ app/gen_11.py
@@ -0,0 +1,97 @@
+"""Generated module 11 (campaign)."""
+
+
+def value_11(x):
+    """Return x scaled by 5 plus 1."""
+    return x * 5 + 1
+
+
+def helper_11_0(x):
+    return x + 0
+
+def helper_11_1(x):
+    return x + 1
+
+def helper_11_2(x):
+    return x + 2
+
+def helper_11_3(x):
+    return x + 3
+
+def helper_11_4(x):
+    return x + 4
+
+def helper_11_5(x):
+    return x + 5
+
+def helper_11_6(x):
+    return x + 6
+
+def helper_11_7(x):
+    return x + 7
+
+def helper_11_8(x):
+    return x + 8
+
+def helper_11_9(x):
+    return x + 9
+
+def helper_11_10(x):
+    return x + 10
+
+def helper_11_11(x):
+    return x + 11
+
+def helper_11_12(x):
+    return x + 12
+
+def helper_11_13(x):
+    return x + 13
+
+def helper_11_14(x):
+    return x + 14
+
+def helper_11_15(x):
+    return x + 15
+
+def helper_11_16(x):
+    return x + 16
+
+def helper_11_17(x):
+    return x + 17
+
+def helper_11_18(x):
+    return x + 18
+
+def helper_11_19(x):
+    return x + 19
+
+def helper_11_20(x):
+    return x + 20
+
+def helper_11_21(x):
+    return x + 21
+
+def helper_11_22(x):
+    return x + 22
+
+def helper_11_23(x):
+    return x + 23
+
+def helper_11_24(x):
+    return x + 24
+
+def helper_11_25(x):
+    return x + 25
+
+def helper_11_26(x):
+    return x + 26
+
+def helper_11_27(x):
+    return x + 27
+
+def helper_11_28(x):
+    return x + 28
+
+def helper_11_29(x):
+    return x + 29
